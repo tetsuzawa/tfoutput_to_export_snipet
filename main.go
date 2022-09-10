8019222e6dc00d49af55f73ad8858834be8de1e4
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2022-09-10 15:13:57 +0900

    bashに対応

--- main.go
+++ main.go
@@ -9,12 +9,15 @@ import (
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
+	builder := &strings.Builder{}
 	for scanner.Scan() {
 		// IP_X = "xxx.xxx.xxx.xxx"
 		rawLine := scanner.Text()
 		line := strings.Replace(rawLine, " ", "", -1)
 		line = strings.Replace(line, "\"", "", -1)
-		line = fmt.Sprintf("export %s", line)
-		fmt.Println(line)
+		builder.WriteString(fmt.Sprintf("export %s", line))
+		builder.WriteString(" && \\\n")
 	}
+	res := strings.TrimSuffix(builder.String(), " && \\\n")
+	fmt.Println(res)
 }
